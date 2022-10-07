de1d7f056546e61dfd96140bd035ca8a6da828d6
omkarNandan <omknan60@gmail.com>
2022-10-08 00:17:54 +0530

    tried making status bar prettier

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-    { cpu_perc, "﬙ %s | ",    NULL },
+    { cpu_perc, " ﬙ %s | ",    NULL },
     { ram_perc, " %s | ",    NULL },
     { disk_used, " %s | ",  "/" },
 	{ datetime, " %s | ",         "%F %T" },
